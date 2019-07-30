9520a1b24fd025b5e3fa24e83dfd04e518a3ea31
chechocardona <39099318+chechocardona@users.noreply.github.com>
2019-07-30 18:11:23 -0500

    Update checkMics.sh
    Added comments and non necessary functions erased

--- checkMics.sh
+++ checkMics.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+# Check if the sound card is recognized
 if arecord -l | grep 'List of CAPTURE Hardware Devices';
 then
+	# Enable gpios
+	# Red LED, recording state
 	echo "16" > /sys/class/gpio/export
-	echo "13" > /sys/class/gpio/export
+	# Yellow LED, processing state // Press Button Confirmation 
 	echo "12" > /sys/class/gpio/export
+	# Play Button. Start Recording
+	echo "13" > /sys/class/gpio/export
+	# Test Button: Record and Play during 3 seconds the selected microphone  
 	echo "6" > /sys/class/gpio/export
+	# Increase Microphone Button: Change the selected microphone for the next one to test it 
 	echo "26" > /sys/class/gpio/export
+	# Stop Recording Switch: Stop recording once the current record and processing are finished
 	echo "7" > /sys/class/gpio/export
+	# Shut Down Button: Safe System shutdown
 	echo "11" > /sys/class/gpio/export
 	
+	# Set In and Out Pins
 	sudo echo "out" > /sys/class/gpio/gpio16/direction
 	sudo echo "out" > /sys/class/gpio/gpio12/direction
 	sudo echo "in" > /sys/class/gpio/gpio13/direction
@@ -16,6 +26,7 @@ then
 	sudo echo "in" > /sys/class/gpio/gpio26/direction
 	sudo echo "in" > /sys/class/gpio/gpio7/direction
 	sudo echo "in" > /sys/class/gpio/gpio11/direction
+	
 	export AUDIODEV=hw:0,0;
 	export AUDIODRIVER=alsa;
 	mic=1;
@@ -28,6 +39,7 @@ then
 		d=$(cat /sys/class/gpio/gpio26/value)
 		e=$(cat /sys/class/gpio/gpio7/value)
 		f=$(cat /sys/class/gpio/gpio11/value)
+		# Start continuous recording
 		if [ $c == "1" ]
 		then
 			let "record = 0"
@@ -48,6 +60,7 @@ then
 				sox /home/pi/TestDirection/recsome.wav /home/pi/TestDirection/"$name"_5.mp3 remix 5;
 				sox /home/pi/TestDirection/recsome.wav /home/pi/TestDirection/"$name"_6.mp3 remix 6;
 				echo "0" > /sys/class/gpio/gpio12/value
+				# Stop Continuous recording
 				e=$(cat /sys/class/gpio/gpio7/value)
 				if [ $e == "1" ]
 				then
@@ -59,6 +72,7 @@ then
 				#sleep 5m;
 			done
 		fi
+		# Start Test of current selected microphone
 		if [ $b == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
@@ -71,6 +85,7 @@ then
 			sox /home/pi/TestDirection/test.wav /home/pi/TestDirection/t.wav remix $mic;
 			aplay -D sysdefault:CARD=audioinjectoroc /home/pi/TestDirection/t.wav
 		fi
+		# Change current selected microphone for the next one
 		if [ $d == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
@@ -84,13 +99,7 @@ then
 			echo "$mic"
 			sleep 1s
 		fi
-		if [ $e == "1" ]
-		then
-			echo "1" > /sys/class/gpio/gpio12/value
-			sleep 1s;
-			echo "0" > /sys/class/gpio/gpio12/value
-			let "record=1"
-		fi
+		# Safe System Shutdown
 		if [ $f == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
@@ -101,14 +110,23 @@ then
 	done
 
 else
+	# Enable gpios
+	# Red LED, recording state
 	echo "16" > /sys/class/gpio/export
-	echo "13" > /sys/class/gpio/export
+	# Yellow LED, processing state // Press Button Confirmation 
 	echo "12" > /sys/class/gpio/export
+	# Play Button. Start Recording
+	echo "13" > /sys/class/gpio/export
+	# Test Button: Record and Play during 3 seconds the selected microphone  
 	echo "6" > /sys/class/gpio/export
+	# Increase Microphone Button: Change the selected microphone for the next one to test it 
 	echo "26" > /sys/class/gpio/export
+	# Stop Recording Switch: Stop recording once the current record and processing are finished
 	echo "7" > /sys/class/gpio/export
+	# Shut Down Button: Safe System shutdown
 	echo "11" > /sys/class/gpio/export
-
+	
+	# Set In and Out Pins
 	sudo echo "out" > /sys/class/gpio/gpio16/direction
 	sudo echo "out" > /sys/class/gpio/gpio12/direction
 	sudo echo "in" > /sys/class/gpio/gpio13/direction
@@ -116,7 +134,8 @@ else
 	sudo echo "in" > /sys/class/gpio/gpio26/direction
 	sudo echo "in" > /sys/class/gpio/gpio7/direction
 	sudo echo "in" > /sys/class/gpio/gpio11/direction
-
+	
+	# Mount Sound Card
  	sudo modprobe -r snd_soc_audioinjector_octo_soundcard;
 	sudo modprobe -r snd_soc_cs42xx8_i2c;
 	sudo modprobe -r snd_soc_cs42xx8;
@@ -135,6 +154,7 @@ else
 		d=$(cat /sys/class/gpio/gpio26/value)
 		e=$(cat /sys/class/gpio/gpio7/value)
 		f=$(cat /sys/class/gpio/gpio11/value)
+		# Start continuous recording
 		if [ $c == "1" ]
 		then
 			let "record = 0"
@@ -154,6 +174,7 @@ else
 				sox /home/pi/TestDirection/recsome.wav /home/pi/TestDirection/"$name"_4.mp3 remix 4;
 				sox /home/pi/TestDirection/recsome.wav /home/pi/TestDirection/"$name"_5.mp3 remix 5;
 				sox /home/pi/TestDirection/recsome.wav /home/pi/TestDirection/"$name"_6.mp3 remix 6;
+				# Stop Continuous recording
 				echo "0" > /sys/class/gpio/gpio12/value
 				e=$(cat /sys/class/gpio/gpio7/value)
 				if [ $e == "1" ]
@@ -166,6 +187,7 @@ else
 				#sleep 5m;
 			done
 		fi
+		# Start Test of current selected microphone
 		if [ $b == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
@@ -177,6 +199,7 @@ else
 			sox /home/pi/TestDirection/test.wav /home/pi/TestDirection/t.wav remix $mic;
 			aplay -D sysdefault:CARD=audioinjectoroc /home/pi/TestDirection/t.wav
 		fi
+		# Change current selected microphone for the next one
 		if [ $d == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
@@ -190,13 +213,7 @@ else
 			echo "$mic"
 			sleep 1s
 		fi
-		if [ $e == "1" ]
-		then
-			echo "1" > /sys/class/gpio/gpio12/value
-			sleep 1s;
-			echo "0" > /sys/class/gpio/gpio12/value
-			let "record=1"
-		fi
+		# Safe System Shutdown
 		if [ $f == "1" ]
 		then
 			echo "1" > /sys/class/gpio/gpio12/value
